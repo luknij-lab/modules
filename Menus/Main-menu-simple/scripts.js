71334e83ff103d18e27f25dbe0d7dc26fcaa9ed7
luknij-lab <lukszozda@gmail.com>
2023-09-29 05:09:40 +0200

    Zmiana pozycji przycisku BBQ oraz częściowe ostylowanie menu w wersji mobilnej

--- Menus/Main-menu-simple/scripts.js
+++ Menus/Main-menu-simple/scripts.js
@@ -3,13 +3,19 @@ document.addEventListener("DOMContentLoaded", () => {
 /*
 BBQ ------------------------------- */
 
+  // button
   const menuBtn = document.querySelector('.hamburger');
+  // menu
+  const mainMenu = document.querySelector('#main-menu');
 
   // Zdarzenie klieknięcia
   menuBtn.addEventListener('click', () => {
 
     // Dodanie sufixu do klasy hamburger elementu <button>
     menuBtn.classList.toggle('hamburger--active');
+
+    // 
+    mainMenu.classList.toggle('mobile--active');
   });
 
 });
