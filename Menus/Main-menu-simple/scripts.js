51a0ef32dda3dcde1e6bb6f47149bc67021cf4e7
luknij-lab <lukszozda@gmail.com>
2023-11-13 23:59:02 +0100

    Praca nad skryptami JS w celu obsługi menu

--- Menus/Main-menu-simple/scripts.js
+++ Menus/Main-menu-simple/scripts.js
@@ -1,32 +1,44 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const menuItems = document.querySelectorAll('.menu-item');
+
+    menuItems.forEach(item => {
+      let isOpen = false;
+
+      item.addEventListener('click', function () {
+        if (!isOpen) {
+          // Open submenu on the first click
+          const submenu = this.querySelector('.submenu');
+          if (submenu) {
+            submenu.style.display = 'block';
+            isOpen = true;
+          }
+        } else {
+          // Navigate to the specified page on the second click
+          const page = this.getAttribute('data-page');
+          if (page) {
+            window.location.href = `${page}.html`; // Change this to your actual page URL
+          }
+        }
+      });
+    });
+  });
+
 document.addEventListener("DOMContentLoaded", function(event) {
 
 /*
 Window width checker -------------- */
 
-// let widthOutput = document.querySelectorAll("#menu-1 .child");
-//
-// window.addEventListener("resize", function(){
-// 	if(window.innerWidth < 768){
-//
-// 		for(let i = 0; i < widthOutput.length; i++){
-// 			let click_count = 0;
-//
-// 			widthOutput[i].addEventListener("click", (e) => {
-//
-// 				click_count += 1;
-//
-// 				if( click_count %2 == 0 ){
-// 					e.default();
-// 				  }else if( click_count >= 3){
-// 					click_count = 1;
-// 					e.preventDefault();
-// 				  }else{
-// 					e.preventDefault();
-// 				  }
-// 			});
-// 		} // end for
-// 	} // end if
-// });
+// let parent_item = document.querySelectorAll("#menu-1 .menu-item-has-children");
+
+// parent_item.forEach( (element) => addEventListener("click", (e) => {
+	
+// 	// let click_aria_change = parent_item.getAttribute('li[aria-expanded]');
+// 	// console.log(click_aria_change);
+
+// 	// if( click_aria_change.getAttribute(false') ){
+// 	// 	e.preventDefault();
+// 	// }
+// }) );
 
 /*
 BBQ ------------------------------- */
