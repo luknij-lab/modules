d3b80a047911fba1cf9aa9d5ae1858d56ea0b6f9
luknij-lab <lukszozda@gmail.com>
2024-04-10 21:33:43 +0200

    Połączenie gałezi

--- Menus/Main-menu-simple/scripts.js
+++ Menus/Main-menu-simple/scripts.js
@@ -5,9 +5,10 @@ document.addEventListener('DOMContentLoaded', function() {
 	let static_window_width = window.innerWidth;
 	const menuItems = document.querySelectorAll('.menu-item-has-children');
 
-	/* Zapobiega domyślemu zdarzeniu kliknięcia w hiperłącze przycisku menu ------------------------------- */
+	/* ------------------------------- */
 	function prevFirstClick(){
 		menuItems.forEach(item => {
+			// z
 			const preventListener = function(event){
 				let isOpen = item.getAttribute('aria-expanded');
 
@@ -23,92 +24,25 @@ document.addEventListener('DOMContentLoaded', function() {
 				}
 			}
 			item.addEventListener('click', preventListener, false );
-		});
+		}); // koniec pętli foreach.
 	};
 
 	// Gdy strona zostanie wyświetlona w wersji mobilnej pierwsze kliknięcie w rodzica, spowoduje otworzenie podmenu, a drugie kliknięcie uruchomi hiperłącze rodzica.
 	if( static_window_width < 768 ){
-<<<<<<< HEAD
-	  let prevFirstClick = (function(){
-		menuItems.forEach(item => {
-			let subNav = document.querySelector('.menu-item .submenu');
-			const menuLink = document.querySelector('.menu-item a');      
-			
-	
-			//////////////////////////
-			// prevent default behavior function
-			const preventListener = function(e){
-				let isOpen = item.getAttribute('aria-expanded');
-	
-				if(isOpen == 'false'){
-					e.preventDefault();
-					item.setAttribute('aria-expanded', 'true');
-				}
-			}
-		
-			item.addEventListener('click', preventListener);
-	
-	
-	
-			//////////////////////////
-		  }); // koniec pętli foreach.
-	  }());
-
-	  document.addEventListener('DOMContentLoaded', prevFirstClick)
-	
-=======
 		window.addEventListener('DOMContentLoaded', prevFirstClick );
->>>>>>> workshop
 	}
 	
 });
 // koniec obsługi statycznej
 
-<<<<<<< HEAD
-	// funkcja zawierająca zdarzenie najechania kursorem myszy na element oraz zdarzenie opuszczenia kursora myszy z obszaru elementu.
-	function handleEvent() {
-		const elements = document.querySelectorAll('.menu-item-has-children');
-		
-		elements.forEach(element => {
-		  // Dodajemy obsługę zmiany aria-expanded przy najechaniu myszką.
-		  element.addEventListener('mouseover', () => {
-			element.setAttribute('aria-expanded', 'true');
-		  });
-  
-		  // Dodajemy obsługę zmiany aria-expanded przy opuszczeniu obszaru elementu.
-		  element.addEventListener('mouseout', () => {
-			element.setAttribute('aria-expanded', 'false');
-		  });
-		});
-	  }
-  
-	// funkcja sprawdzająca szerokość ekranu i gdy szerokość jest >= 768 px to rejestruje funkcję handleEvent, a gdy szerokość ekranu jest < 768 px to usuwa funkcję handleEvent.
-	function checkWindowSize() {
-		window.addEventListener('resize', function(){
-			let dynamic_window_width = this.innerWidth;
-	  
-			if( dynamic_window_width >= 768 ){
-				window.addEventListener('resize', handleEvent);
-				//item.removeEventListener('click', preventListener);
-			}else{
-				window.removeEventListener('resize', handleEvent);
-			}
-		});
-	  }
-  
-	// uruchomienie funkcji
-	checkWindowSize();
-	window.addEventListener('resize', checkWindowSize);
-=======
 // Obsługa dynamiczna
 window.addEventListener('resize', function(){
->>>>>>> workshop
 
 	const menuItems = document.querySelectorAll('.menu-item-has-children');
 	let dynamic_window_width = this.innerWidth;
 	console.log(dynamic_window_width);
 
-	/* Zapobiega domyślemu zdarzeniu kliknięcia w hiperłącze przycisku menu ------------------------------- */
+	/* ------------------------------- */
 	function prevFirstClick(){
 		menuItems.forEach(item => {
 			// prevent default behavior function
@@ -139,6 +73,7 @@ window.addEventListener('resize', function(){
 			let isOpen = item.getAttribute('aria-expanded');
 	
 			if (isOpen == 'true') {
+				// first click on parent opens submenu
 				item.setAttribute('aria-expanded', 'false');
 			}
 		});
@@ -146,54 +81,55 @@ window.addEventListener('resize', function(){
 	}
 
 });
+// koniec obsługi dynamicznej
 
-  /* BBQ ------------------------------- */
-  
-  document.addEventListener("DOMContentLoaded", function(event) {
-  
-	// Zmienne - ciało HTML
-	const body_html = document.querySelector('body');
-	const top_menu = document.querySelector('.main-navigation');
-  
-	// Zmienne - BBQ
-	const bbq = document.querySelector('.bbq');
-	const bbq_bar = document.querySelector('.bbq-bar');
-  
-	// BBQ zdarzenie kliknięcia
-	bbq.addEventListener("click", function(){
-  
-	  // zmiana wyglądu przycisku
-	  bbq_bar.classList.toggle("active-btn");
-	  body_html.classList.toggle("openedMobileMenu");
-  
-	  // // otwieranie i zamykanie menu gdy BBQ zostanie wciśnięty
-	  top_menu.classList.toggle("mobile");
-  
-	  // zamknięcie wszystkich otwartych submenu gdy zamknie się całe menu przyciskiem BBQ
-	  const menuItems = document.querySelectorAll('.menu-item-has-children');
-  
-	  if(bbq_bar.classList.contains('active-btn') == true){
-		menuItems.forEach(item => {
-		  let isOpen = item.getAttribute('aria-expanded');
-		
-		  if (isOpen == 'true') {
-			item.setAttribute('aria-expanded', 'false');
-		  }
-		});
-	  }
+/* BBQ ------------------------------- */
+
+document.addEventListener("DOMContentLoaded", function(event) {
+
+// Zmienne - ciało HTML
+const body_html = document.querySelector('body');
+const top_menu = document.querySelector('.main-navigation');
+
+// Zmienne - BBQ
+const bbq = document.querySelector('.bbq');
+const bbq_bar = document.querySelector('.bbq-bar');
+
+// BBQ zdarzenie kliknięcia
+bbq.addEventListener("click", function(){
+
+	// zmiana wyglądu przycisku
+	bbq_bar.classList.toggle("active-btn");
+	body_html.classList.toggle("openedMobileMenu");
+
+	// // otwieranie i zamykanie menu gdy BBQ zostanie wciśnięty
+	top_menu.classList.toggle("mobile");
+
+	// zamknięcie wszystkich otwartych submenu gdy zamknie się całe menu przyciskiem BBQ
+	const menuItems = document.querySelectorAll('.menu-item-has-children');
+
+	if(bbq_bar.classList.contains('active-btn') == true){
+	menuItems.forEach(item => {
+		let isOpen = item.getAttribute('aria-expanded');
+	
+		if (isOpen == 'true') {
+		item.setAttribute('aria-expanded', 'false');
+		}
 	});
-  
-	/* Zmiana szerokości - czyszczenie klas ------------------------------- */
-	// zamyka wszystkie menu gdy menu jest otwarte a użytkownik zmienił rozmiar ekranu na większy niż 768 px
-	window.addEventListener('resize', function(){
-	  let win_width = this.innerWidth;
-  
-	  if( win_width > 768 ){
-		body_html.classList.remove("openedMobileMenu");
-		top_menu.classList.remove("mobile");
-  
-		// BBQ przycisk
-		bbq_bar.classList.remove("active-btn");
-	  }
-	});
-  }); // koniec DOMContentLoaded
+	}
+});
+
+/* Zmiana szerokości - czyszczenie klas ------------------------------- */
+// zamyka wszystkie menu gdy menu jest otwarte a użytkownik zmienił rozmiar ekranu na większy niż 768 px
+window.addEventListener('resize', function(){
+	let win_width = this.innerWidth;
+
+	if( win_width > 768 ){
+	body_html.classList.remove("openedMobileMenu");
+	top_menu.classList.remove("mobile");
+
+	// BBQ przycisk
+	bbq_bar.classList.remove("active-btn");
+	}
+});
+}); // koniec DOMContentLoaded
